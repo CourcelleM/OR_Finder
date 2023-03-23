c80b372df146805f16062e89137b817b6072f643
Courcelle Maxime <courcellem@muse-login02.cluster>
2023-03-23 16:58:35 +0100

    Added expected number of jobs to test_run.sh file

--- test_run.sh
+++ test_run.sh
@@ -5,3 +5,5 @@ module load snakemake/7.15.1-conda
 
 snakemake -s scripts/OR_script.snake --cores 1 --use-conda --configfile scripts/config.yaml --cluster-config scripts/cluster.yaml --cluster "sbatch -J {cluster.job-name} -A agap -p {cluster.queue} {cluster.time} {cluster.nodes} {cluster.cpus} {cluster.out}" --jobs 100 --dry-run --reason
 if [ $loaded == "no" ] ; then module unload snakemake/7.15.1-conda ; fi
+
+echo 'Expected number of jobs for a new genome: 67 (+1 for the "All" rule)'
