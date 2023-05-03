5e4bb04e83a0208f86ff0749813ca342df0e7b34
Courcelle Maxime <courcellem@muse-login01.cluster>
2023-05-03 09:28:32 +0200

    Asks whether to run the script locally or on a computing cluster

--- run_snake.sh
+++ run_snake.sh
@@ -5,4 +5,19 @@ module load snakemake/7.15.1-conda
 if ! [ -d Results/Intermediary_files ] ; then mkdir -p Results/Intermediary_files ; fi
 if ! [ -d Logs ] ; then mkdir -p Logs ; fi
 
-snakemake -s scripts/OR_script.snake --cores 1 --use-conda --configfile scripts/config.yaml --cluster-config scripts/cluster.yaml --cluster "sbatch -J {cluster.job-name} -A agap -p {cluster.queue} {cluster.time} {cluster.mem} {cluster.nodes} {cluster.cpus} {cluster.out}" --jobs 100
+echo -e "Will the script run on a computing cluster ? (y/n)"
+read ANSWER
+
+case $ANSWER in
+	y)
+		echo "Using the cluster.yaml config file."
+		snakemake -s scripts/OR_script.snake --cores 1 --use-conda --configfile scripts/config.yaml --cluster-config scripts/cluster.yaml --cluster "sbatch -J {cluster.job-name} -A agap -p {cluster.queue} {cluster.time} {cluster.mem} {cluster.nodes} {cluster.cpus} {cluster.out}" --jobs 100
+		;;
+	n)
+		echo "Local run."
+		snakemake -s scripts/OR_script.snake --cores 1 --use-conda --configfile scripts/config.yaml
+		;;
+	*)
+		echo "Unknown answer."
+		;;
+esac
--- test_run.sh
+++ test_run.sh
@@ -3,7 +3,7 @@
 if module list -t 2>&1 | grep -q snakemake ; then loaded="yes" ; else loaded="no" ; fi
 module load snakemake/7.15.1-conda
 
-snakemake -s scripts/OR_script.snake --cores 1 --use-conda --configfile scripts/config.yaml --cluster-config scripts/cluster.yaml --cluster "sbatch -J {cluster.job-name} -A agap -p {cluster.queue} {cluster.time} {cluster.nodes} {cluster.cpus} {cluster.out}" --jobs 100 --dry-run --reason
+snakemake -s scripts/OR_script.snake --cores 1 --use-conda --configfile scripts/config.yaml --cluster-config scripts/cluster.yaml --cluster "sbatch -J {cluster.job-name} -A agap -p {cluster.queue} {cluster.time} {cluster.nodes} {cluster.cpus} {cluster.out}" --jobs 100 --dry-run 
 if [ $loaded == "no" ] ; then module unload snakemake/7.15.1-conda ; fi
 
 echo 'Expected number of jobs for a new genome: 70 (+1 for the "All" rule)'
