88ed3a12389c4234afa0770c983cc4e3593c955c
Courcelle Maxime <courcellem@muse-login01.cluster>
2023-05-10 12:43:26 +0200

    Adapted test_run.sh to account for singularity and new config files execution.

--- test_run.sh
+++ test_run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if module list -t 2>&1 | grep -q snakemake ; then loaded="yes" ; else loaded="no" ; fi
-module load snakemake/7.15.1-conda
+module load snakemake
 
-snakemake -s scripts/OR_script.snake --cores 1 --configfile scripts/config.yaml --cluster-config scripts/cluster.yaml --cluster "sbatch -J {cluster.job-name} -A agap -p {cluster.queue} {cluster.time} {cluster.nodes} {cluster.cpus} {cluster.out}" --jobs 100 --dry-run 
-if [ $loaded == "no" ] ; then module unload snakemake/7.15.1-conda ; fi
+snakemake --profile profiles/CLUSTER --dry-run 
+if [ $loaded == "no" ] ; then module unload snakemake ; fi
 
-echo 'Expected number of jobs for a new genome: 70 (+1 for the "All" rule)'
+echo 'Expected number of jobs for a new genome: 68 (+1 for the "All" rule)'
